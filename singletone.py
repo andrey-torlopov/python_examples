d8d3c454fe09d4eff44ffd04af38949a3d9a13cc
Andrey Torlopov <torlopov.mail@ya.ru>
2023-06-10 15:01:19 +0300

    singletone example

---
+++ singletone.py
@@ -0,0 +1,19 @@
+# Пример с синглтоном
+
+class Singletone:
+    instanse = None
+
+    def __new__(cls):
+        if cls.instanse is None:
+            cls.instanse = super().__new__(cls)
+        return cls.instanse
+
+
+class Foo(Singletone):
+    pass
+
+
+a = Foo()
+b = Foo()
+print(a is b)
+print(id(a), id(b))
