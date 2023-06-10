105f75e7a54d352c6ddb6a02bc584663365c4d12
Andrey Torlopov <torlopov.mail@ya.ru>
2023-06-10 14:54:19 +0300

    add doctest example

---
+++ doc_test_example.py
@@ -0,0 +1,18 @@
+
+def foo(x):
+    """
+    Тестируем метод
+    >>> a = 3
+    >>> b = 5
+    >>> a == b
+    False
+    >>> foo(3) == 3
+    True
+    """
+    return x
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
