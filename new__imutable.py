1e96f8b6e7d4c63cf614acddc641c633d3edb172
Andrey Torlopov <torlopov.mail@ya.ru>
2023-06-10 20:54:40 +0300

    add custom tuple

--- new__imutable.py
+++ new__imutable.py
@@ -1,3 +1,15 @@
-# наследник от Tuple
+class CustomTuple(tuple):
+    def __new__(cls, *args, **kwargs):
+        instance = super().__new__(cls, args)
+        instance.property1 = kwargs.get('property1', None)
+        instance.property2 = kwargs.get('property2', None)
+        return instance
 
-# пример...
+
+# Пример использования
+t = CustomTuple(1, 2, 3, property1='value1', property2='value2')
+
+print(t)  # (1, 2, 3)
+
+print(t.property1)  # value1
+print(t.property2)  # value2
