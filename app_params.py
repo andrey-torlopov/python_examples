ca540422626da2d3b6046563a9a9e14e6d73bb6e
Andrey Torlopov <torlopov.mail@ya.ru>
2023-06-10 14:48:27 +0300

    add app param example

---
+++ app_params.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python3
+import sys
+from datetime import datetime
+
+# Принимаем параметры из командной строки
+
+def main(args):
+    ans = 1
+    for arg in args[1:]:
+        ans *= int(arg)
+    print("calculated result as: {} on: {} ".format(ans, datetime.now()))
+
+
+if __name__ == '__main__':
+    main(sys.argv)
